4e9c3851c31586fe065d544afb12512f942f3fa2
Janriel Testado <2150048@slu.edu.ph>
2018-05-24 21:04:42 +0800

    Edited Teeth model

--- app/Teeth.php
+++ app/Teeth.php
@@ -5,7 +5,21 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Teeth extends Model
-{
+{   
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'teeth';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'teethID';
+
     public function schedule()
     {
         return $this->belongsTo('App\Schedule');
