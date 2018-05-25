94a318d484110625cb5bc8f7d06f0e6c8783841f
Ramon Lopez <2155703@slu.edu.ph>
2018-05-25 15:34:20 +0800

    updated index

--- resources/views/Assistant/schedules/index.blade.php
+++ resources/views/Assistant/schedules/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Schedule
+           Active Schedules
         </h1>
         <ol class="breadcrumb">
             <li><a href="/assistant"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -71,6 +71,143 @@
                         </table>
                         <div class="pagination-wrapper"> {!! $schedules->appends(['search' => Request::get('search')])->render() !!} </div>
                     </div>
+
+                    
+                </div>
+            </div>
+            </div>
+        </div>
+        <section class="content-header">
+            <h1>
+                Done Schedules
+            </h1>
+        </section>
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <br>
+                <div class="box">
+                <div class="box-body">
+                    <form method="GET" action="{{ url('/assistant/schedules') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
+                        <div class="input-group pull-right">
+                            <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
+                        </div>
+                        <span class="input-group-append pull-right">
+                            <button class="btn btn-secondary" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </form>
+                    <br>
+                    <br>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Patient Name</th>
+                                    <th>Date</th>
+                                    <th>Time From</th>
+                                    <th>Time To</th>
+                                    <th>Services</th>
+                                    <th>Dentist</th>
+                                    <th>Operation Status</th>
+                                    <th><center>Actions</center></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($schedules as $item)
+                            @if($item->opStatus == 'Done')
+                                <tr>
+                                    <td>{{ $item->patient->name }}</td>
+                                    <td>{{ $item->date }}</td>
+                                    <td>{{ $item->timeFrom }}</td>
+                                    <td>{{ $item->timeTo }}</td>
+                                    <td>{{ $item->service->servName }}</td>
+                                    <td>{{ $item->dentist->name }}</td>
+                                    <td>{{ $item->opStatus }}</td>
+                                    <td>
+                                        <center>
+                                        <a href="{{ url('/assistant/schedules/' . $item->schedId . '/view') }}" title="View Schedule"><button class="btn btn-success btn-sm" onclick="return confirm(&quot;Confirm delete?&quot;"><i class="fa fa-check" aria-hidden="true"></i> Checkout</button></a>
+
+                                    </center>
+                                    </td>
+                                </tr>
+                                @endif
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="pagination-wrapper"> {!! $schedules->appends(['search' => Request::get('search')])->render() !!} </div>
+                    </div>
+
+                    
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <section class="content-header">
+            <h1>
+                Ongoing Schedules
+            </h1>
+        </section>
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <br>
+                <div class="box">
+                <div class="box-body">
+                    <form method="GET" action="{{ url('/assistant/schedules') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
+                        <div class="input-group pull-right">
+                            <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
+                        </div>
+                        <span class="input-group-append pull-right">
+                            <button class="btn btn-secondary" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </form>
+                    <br>
+                    <br>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Patient Name</th>
+                                    <th>Date</th>
+                                    <th>Time From</th>
+                                    <th>Time To</th>
+                                    <th>Services</th>
+                                    <th>Dentist</th>
+                                    <th>Operation Status</th>
+                                    <th><center>Actions</center></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($schedules as $item)
+                            @if($item->opStatus == 'Ongoing')
+                                <tr>
+                                    <td>{{ $item->patient->name }}</td>
+                                    <td>{{ $item->date }}</td>
+                                    <td>{{ $item->timeFrom }}</td>
+                                    <td>{{ $item->timeTo }}</td>
+                                    <td>{{ $item->service->servName }}</td>
+                                    <td>{{ $item->dentist->name }}</td>
+                                    <td>{{ $item->opStatus }}</td>
+                                    <td>
+                                        <center>
+                                        <a href="{{ url('/assistant/schedules/' . $item->schedId . '/view') }}" title="View Schedule"><button class="btn btn-success btn-sm" onclick="return confirm(&quot;Confirm delete?&quot;"><i class="fa fa-check" aria-hidden="true"></i> Checkout</button></a>
+
+                                    </center>
+                                    </td>
+                                </tr>
+                                @endif
+                            @endforeach
+                            </tbody>
+                        </table>
+                        <div class="pagination-wrapper"> {!! $schedules->appends(['search' => Request::get('search')])->render() !!} </div>
+                    </div>
+
+                    
                 </div>
             </div>
             </div>
